625072e2efe6fb81c2834b6e8b6dfea6d26f9e9d
TheTonyk <lebrun.noe@gmail.com>
2016-05-02 00:29:57 +0200

    Added greatest message if you are already in the hub.

--- ComandsBungee/src/com/thetonyk/CommandsBungee/Commands/HubCommand.java
+++ ComandsBungee/src/com/thetonyk/CommandsBungee/Commands/HubCommand.java
@@ -16,6 +16,8 @@ public class HubCommand extends Command {
 	@Override
     public void execute(CommandSender sender, String[] args) {
 		
+		if (Main.proxy.getProxy().getPlayer(sender.getName()).getServer().getInfo().getName().equalsIgnoreCase("hub")) sender.sendMessage(Main.prefix().append("You are already in the hub.").create());
+		
 		Main.proxy.getProxy().getPlayer(sender.getName()).connect(Main.proxy.getProxy().getServerInfo("hub"));
     	
     }
