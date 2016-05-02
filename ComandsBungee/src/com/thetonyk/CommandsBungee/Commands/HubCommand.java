5627909f63bf57e56790e9764e3597838027ed5f
TheTonyk <lebrun.noe@gmail.com>
2016-05-02 21:18:56 +0200

    Added color to /hub error.

--- ComandsBungee/src/com/thetonyk/CommandsBungee/Commands/HubCommand.java
+++ ComandsBungee/src/com/thetonyk/CommandsBungee/Commands/HubCommand.java
@@ -2,6 +2,7 @@ package com.thetonyk.CommandsBungee.Commands;
 
 import com.thetonyk.CommandsBungee.Main;
 
+import static net.md_5.bungee.api.ChatColor.*;
 import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.plugin.Command;
 
@@ -16,7 +17,7 @@ public class HubCommand extends Command {
 	@Override
     public void execute(CommandSender sender, String[] args) {
 		
-		if (Main.proxy.getProxy().getPlayer(sender.getName()).getServer().getInfo().getName().equalsIgnoreCase("hub")) sender.sendMessage(Main.prefix().append("You are already in the hub.").create());
+		if (Main.proxy.getProxy().getPlayer(sender.getName()).getServer().getInfo().getName().equalsIgnoreCase("hub")) sender.sendMessage(Main.prefix().append("You are already in the hub.").color(GRAY).create());
 		
 		Main.proxy.getProxy().getPlayer(sender.getName()).connect(Main.proxy.getProxy().getServerInfo("hub"));
     	
